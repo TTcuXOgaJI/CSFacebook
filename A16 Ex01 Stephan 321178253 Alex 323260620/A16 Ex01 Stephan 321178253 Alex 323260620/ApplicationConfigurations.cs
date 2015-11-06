952cd0fb0253d6c5dd57dfb7738d3c590ac3842a
Glaider <avHITlearn@gmail.com>
2015-11-06 11:03:21 +0200

    Created ApplicationConfigurations.cs

---
+++ A16 Ex01 Stephan 321178253 Alex 323260620/A16 Ex01 Stephan 321178253 Alex 323260620/ApplicationConfigurations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace A16_Ex01_Stephan_321178253_Alex_323260620
+{
+    class ApplicationConfigurations
+    {
+        private static readonly string sr_FileName;
+
+        static ApplicationConfigurations()
+        {
+            sr_FileName = Application.ExecutablePath + ".configurations.xml";
+        }
+
+
+
+        private ApplicationConfigurations()
+        {
+        }
+
+
+        private static ApplicationConfigurations s_ThisApplicationConfigurations;
+
+
+        public static ApplicationConfigurations instanse
+        {
+            get
+            {
+                if (s_ThisApplicationConfigurations == null)
+                {
+
+
+                }
+
+                return null;
+            }
+        }
+    }
+}
