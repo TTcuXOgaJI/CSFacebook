89a54c37e5565e0fe5f156e3dbfb4b25eb835843
Stephan89s <stephan1989s@gmail.com>
2015-11-07 13:14:51 +0200

    fxdf

--- A16 Ex01 Stephan 321178253 Alex 323260620/A16 Ex01 Stephan 321178253 Alex 323260620/FormFacebookAccountBoard.cs
+++ A16 Ex01 Stephan 321178253 Alex 323260620/A16 Ex01 Stephan 321178253 Alex 323260620/FormFacebookAccountBoard.cs
@@ -139,10 +139,7 @@ namespace A16_Ex01_Stephan_321178253_Alex_323260620
 
         private void pictureBoxUserSmallPicture_Click(object sender, EventArgs e)
         {
-            if(true)
-            {
-
-            }
+            
         }
     }
 }
